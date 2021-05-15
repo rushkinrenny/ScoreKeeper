f8b4d8412723e8198b2a9e5dc03a088f06192ecf
rushkinrenny <kumarakash0407@gmail.com>
2021-05-15 16:36:02 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const p1 = {
-    score=0,
-    button=document.querySelector('#p1Button'),
-    display=document.querySelector('#p1Display')
+    score:0,
+    button:document.querySelector('#p1Button'),
+    display:document.querySelector('#p1Display')
 }
 const p2 = {
-    score=0,
-    button=document.querySelector('#p2Button'),
-    display=document.querySelector('#p2Display')
+    score:0,
+    button:document.querySelector('#p2Button'),
+    display:document.querySelector('#p2Display')
 }
 
 const resetButton = document.querySelector('#reset');
